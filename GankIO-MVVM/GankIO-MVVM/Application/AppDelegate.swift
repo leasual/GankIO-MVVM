1a064221f4a42aac279af2f2cc6edf0fb196d6c9
james.li <leasual@gmail.com>
2019-06-20 23:45:06 +0800

    add Moya interface and GankIO's API.

---
+++ GankIO-MVVM/GankIO-MVVM/Application/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  GankIO-MVVM
+//
+//  Created by james.li on 2019/6/20.
+//  Copyright © 2019 james.li. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = .white
+        self.window?.rootViewController = DependencyContainer.resolve(MainController.self)
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+       
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+       
+    }
+
+
+}
+
---
+++ GankIO-MVVM/GankIO-MVVMTests/GankIO_MVVMTests.swift
@@ -0,0 +1,34 @@
+//
+//  GankIO_MVVMTests.swift
+//  GankIO-MVVMTests
+//
+//  Created by james.li on 2019/6/20.
+//  Copyright © 2019 james.li. All rights reserved.
+//
+
+import XCTest
+@testable import GankIO_MVVM
+
+class GankIO_MVVMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
